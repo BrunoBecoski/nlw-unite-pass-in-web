c8ba053969dcb5a304fcbfd138daea5fcc9360c4
Bruno Becoski <brunobecoski@gmail.com>
2024-04-07 12:45:17 -0300

    Cria a funcionalidade de URL State

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -25,9 +25,26 @@ interface Attendee {
 const eventId = 'cb9108f2-8d99-4d30-bfa1-bb6e3bb41da0'
 
 export function AttendeeList() {
-  const [search, setSearch] = useState('')
-  const [page, setPage] = useState(1)
+  const [search, setSearch] = useState(() => {
+    const url = new URL(window.location.toString())
 
+    if (url.searchParams.has('search')) {
+      return url.searchParams.get('search') ?? ''
+    }
+
+    return ''
+  })
+
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if (url.searchParams.has('page')) {
+      return Number(url.searchParams.get('page'))
+    }
+
+    return 1
+  })
+  
   const [total, setTotal] = useState(0)
   const [attendees, setAttendees] = useState<Attendee[]>([])
 
@@ -50,25 +67,46 @@ export function AttendeeList() {
       })
   }, [page, search])
 
+
+  function setCurrentSearch(search: string) {
+    const url = new URL(window.location.toString())
+
+    url.searchParams.set('search', search)
+
+    window.history.pushState({}, '', url)
+
+    setSearch(search)
+  }
+
+  function setCurrentPage(page: number) {
+    const url = new URL(window.location.toString())
+    
+    url.searchParams.set('page', String(page))
+
+    window.history.pushState({}, '', url)
+
+    setPage(page)
+  }
+
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
-    setSearch(event.target.value)
-    setPage(1)
+    setCurrentSearch(event.target.value)
+    setCurrentPage(1)
   }
 
   function goToFirstPage() {
-    setPage(1);
+    setCurrentPage(1)
   }
   
   function goToPreviousPage() {
-    setPage(page - 1)
+    setCurrentPage(page - 1)
   }
 
   function goToNextPage() {
-    setPage(page + 1)
+    setCurrentPage(page + 1)
   }
 
   function goToLastPage() {
-    setPage(totalPages)
+    setCurrentPage(totalPages)
   }
 
   return (
