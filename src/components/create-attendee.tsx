30bf008bfd0ad74c3a353a2de835b58a1421cb56
Bruno Becoski <brunobecoski@gmail.com>
2024-06-04 21:54:07 -0300

    Atualiza o componente CreateAttendee

--- src/components/create-attendee.tsx
+++ src/components/create-attendee.tsx
@@ -1,4 +1,10 @@
 import { FormEvent } from 'react'
+import * as  z from 'zod'
+
+const schema = z.object({
+  name: z.string().min(3),
+  email: z.string().email(),
+})
 
 export function CreateAttendee() {
   
@@ -8,17 +14,26 @@ export function CreateAttendee() {
     const name = event.target['name'].value
     const email = event.target['email'].value
 
-    console.log('Name: ' + name)
-    console.log('Email: ' + email)
+    const result = schema.safeParse({
+      name,
+      email,
+    })
+
+    if (result.success == false) {
+      alert(result.error)
+      return
+    }
+      
+    console.log(result)
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="name">Nome: </label>
-      <input id="name" />
+      <input id="name" className="text-black" />
 
       <label htmlFor="email">Email: </label>
-      <input id="email" />
+      <input id="email" className="text-black" />
 
       <button type="submit">Criar participante</button>
     </form>
