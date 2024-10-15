e55db1f3dfe53447e07103e7b8e4b0c97ae54859
Bruno Becoski <brunobecoski@gmail.com>
2024-10-14 21:37:08 -0300

    Atualiza o componente AttendeeDetails

--- src/components/attendees/attendee-details.tsx
+++ src/components/attendees/attendee-details.tsx
@@ -1,6 +1,14 @@
 import { useEffect, useState } from "react";
+import dayjs from "dayjs";
+
 import { useRouter } from "../../contexts/router-provider";
 import { AttendeeAndEventsType, getAttendee } from "../../fetches";
+
+import { Table } from "../table/table";
+import { TableHeader } from "../table/table-header";
+import { TableCell } from "../table/table-cell";
+import { TableRow } from "../table/table-row";
+import { MoreButton } from "../buttons/more-button";
 
 export function AttendeeDetails() {
   const [attendee, setAttendee] = useState<AttendeeAndEventsType>({} as AttendeeAndEventsType)
@@ -27,11 +35,76 @@ export function AttendeeDetails() {
   }, [])
 
   return (
-    <div>
-      <h1>Participante {attendee.name}</h1>
-      <pre>
-        {JSON.stringify(attendee, null, ' ')}
-      </pre>
+    <div className="space-y-6">
+      <div className="flex justify-between">
+        <div>
+          <h1 className="text-2xl font-bold">{attendee.name}</h1>
+          <p>{attendee.email}</p>
+        </div>
+      </div>
+
+      <h2 className="text-2xl font-semibold">Eventos</h2>
+
+      {attendee.events &&
+        <Table>
+          <thead>
+            <tr className="border-b border-white/10">
+              <TableHeader style={{ width: 48 }} >
+                <input className="size-4 bg-black/20 rounded border border-white/10 cursor-pointer checked:bg-orange-400" type="checkbox" />
+              </TableHeader>
+              <TableHeader>Slug</TableHeader>
+              <TableHeader>Evento</TableHeader>
+              <TableHeader>Começa</TableHeader>
+              <TableHeader>Termina</TableHeader>
+              <TableHeader>Confirmado</TableHeader>
+              <TableHeader style={{ width: 64 }}></TableHeader>
+            </tr>
+          </thead>
+
+          <tbody>
+            {attendee.events.map((event) => {
+              return (
+                <TableRow key={event.id}>
+                  <TableCell>
+                    <input className="size-4 bg-black/20 rounded border border-white/10 cursor-pointer checked:bg-orange-400" type="checkbox" />
+                  </TableCell>
+                  
+                  <TableCell>
+                    {event.slug}
+                  </TableCell>
+
+                  <TableCell>
+                    <div className="flex flex-col gap-1">
+                      <span className="font-semibold text-white">{event.title}</span>
+                      <span>{event.details}</span>
+                    </div>
+                  </TableCell>
+
+                  <TableCell>
+                    {dayjs(event.startDate).format('DD/MM/YY')}
+                  </TableCell>
+
+                  <TableCell>
+                    {dayjs(event.endDate).format('DD/MM/YY')}
+                  </TableCell>
+
+                  <TableCell>
+                    {event.checkIn ? 'Sim' : 'Não'}
+
+                  </TableCell>
+
+                  <TableCell>
+                    <MoreButton 
+                      slug={event.slug}
+                      variant="event"
+                    />
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </tbody>
+        </Table>
+      }
     </div>
   )
 }
