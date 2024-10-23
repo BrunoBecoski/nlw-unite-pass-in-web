b57d360c6594fcd90753d72b38e039e83e2085df
Bruno Becoski <brunobecoski@gmail.com>
2024-10-22 22:31:52 -0300

    Atualiza o componente Input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,45 +1,41 @@
-import { useRef } from 'react'
+import { Component, useRef } from 'react'
+import { tv, type VariantProps } from 'tailwind-variants'
 
 import { IconButton } from './buttons/icon-button'
 import { Icon, IconName } from './icon'
-import { twMerge } from 'tailwind-merge'
 
 export type InputVariants = 'default' | 'success' | 'error'
 
-interface FormInputProps {
+const input = tv({
+  base: 'px-3 py-1.5 border rounded-lg text-sm flex items-center gap-3',
+
+  variants: {
+    variant: {
+      primary: 'border-orange-200/50 focus-within:border-orange-500 text-orange-300 focus-within:text-orange-500',
+      success: 'border-emerald-200/50 focus-within:border-emerald-500 text-emerald-300 focus-within:text-emerald-500',
+      error: 'border-red-200/50 focus-within:border-red-500 text-red-300 focus-within:text-red-500',
+    }
+  }
+})
+
+interface InputProps extends Component<'input'>, VariantProps<typeof input> {
   id: string
   label: string
   iconName: IconName
   message?: string
-  variant?: InputVariants
 }
 
 export function FormInput({ 
   id,
   label,
   iconName,
-  message,
-  variant = 'default',
-}: FormInputProps) {
-
-  let styles = ''
-
-  switch (variant) {
-    case 'success': styles = 'border-emerald-200/50 focus-within:border-emerald-500 text-emerald-300 focus-within:text-emerald-500'
-      break;
-
-    case 'error': styles = 'border-red-200/50 focus-within:border-red-500 text-red-300 focus-within:text-red-500'
-      break;
-  
-    default: styles = 'border-orange-200/50 focus-within:border-orange-500 text-orange-300 focus-within:text-orange-500'
-      break;
-  }
-
+  variant,
+}: InputProps) {
   return (
     <div className="flex flex-col gap-2 w-min">
       <label htmlFor={id}>{label}</label>
 
-      <div className={twMerge('px-3 py-1.5 border rounded-lg text-sm flex items-center gap-3', styles)}>
+      <div className={input({ variant })}>
         <Icon 
           name={iconName} 
           size="sm"
@@ -50,31 +46,7 @@ export function FormInput({
           name={id}
           className="bg-transparent text-white flex-1 outline-none border-0 p-0 text-sm focus:ring-0"
         />
-
-        { message && variant == 'success' &&
-          <Icon 
-            name="circle-check"
-            size="sm"
-            color="emerald"
-          />
-        }
-
-        { message && variant == 'error' &&
-          <Icon 
-            name="circle-alert"
-            size="sm"
-            color="red"
-          />
-        }
       </div>
-
-      { message && variant == 'success' &&
-        <span className="text-emerald-400 text-sm text-right">{message}</span>
-      }
-
-      { message && variant == 'error' &&
-        <span className="text-red-400 text-sm text-right">{message}</span>
-      }
     </div>
   )
 }
