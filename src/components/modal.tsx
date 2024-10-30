9218c243159041210448461acd0d6e41db66de4d
Bruno Becoski <brunobecoski@gmail.com>
2024-10-29 22:06:59 -0300

    Atualiza o componente Modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react'
 import { createPortal } from 'react-dom'
-import { tv, type VariantProps } from 'tailwind-variants'
 
 import { Button } from './button'
 import { IconButton } from './buttons/icon-button'
@@ -14,35 +13,13 @@ export type ModalData = {
   attendee?: AttendeeTypes
 }
 
-const modal = tv({
-  slots: {
-    div_1: 'fixed inset-0 flex items-center justify-center',
-    div_2: 'flex flex-col items-center justify-between min-w-96 min-h-96 bg-zinc-950 text-white p-6 rounded-lg relative border',
-    title: 'text-xl font-bold my-4 text-center'
-  },
-
-  variants: {
-    variant: {
-      default: 'bg-orange-950/50 border-orange-500 text-orange-500',
-      success: 'bg-emerald-950/50 border-emerald-500 text-emerald-50',
-      error: 'bg-red-950/50 border-red-500 text-red-500',
-    }
-  },
-
-  defaultVariants: {
-    variant: 'default',
-  }
-})
-
-interface ModalProps extends VariantProps<typeof modal> {
+interface ModalProps {
   showModal: boolean
   handleCloseModal: () => void
   data: ModalData
 }
 
-export function Modal({ data, showModal, handleCloseModal, variant,    }: ModalProps) {
-
-  const { div_1, div_2, title  } = modal({ variant })
+export function Modal({ data, showModal, handleCloseModal }: ModalProps) {
 
   useEffect(() => {
     if (showModal == true) {
@@ -56,8 +33,8 @@ export function Modal({ data, showModal, handleCloseModal, variant,    }: ModalP
     <div>
       { showModal &&
         createPortal(
-          <div className={div_1()}>
-            <div className={div_2()}>
+          <div className="fixed inset-0 flex items-center justify-center bg-black/50">
+            <div className="flex flex-col items-center justify-between min-w-96 min-h-96 bg-zinc-950 text-white p-6 rounded-lg relative border border-orange-500">
               <IconButton 
                 name="x"
                 onClick={handleCloseModal}
@@ -66,7 +43,7 @@ export function Modal({ data, showModal, handleCloseModal, variant,    }: ModalP
                 className="absolute top-2 right-2"
               />
 
-                <h2 className={title()}>
+                <h2 className="text-xl font-bold my-4 text-center">
                   {data.title}
                 </h2>
 
@@ -80,19 +57,8 @@ export function Modal({ data, showModal, handleCloseModal, variant,    }: ModalP
                   </>
                 }
 
-                {data.variant == 'default' &&
-                  <Button onClick={handleCloseModal}>{data.button}</Button>
-                }
-
-                {data.variant == 'success' &&
-                  <Button onClick={handleCloseModal}>{data.button}</Button>
-                }
-
-                {data.variant == 'error' &&
-                  <Button onClick={handleCloseModal}>{data.button}</Button>
-                }
+                <Button onClick={handleCloseModal}>{data.button}</Button>
               </div>
-
           </div>,
           document.body
         )
