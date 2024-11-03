e62ffb07cdf795818dbdae05956a21498864f9ba
Bruno Becoski <brunobecoski@gmail.com>
2024-11-02 21:26:52 -0300

    Atualiza o componente EventForm

--- src/components/events/event-form.tsx
+++ src/components/events/event-form.tsx
@@ -1,37 +1,89 @@
+import { FormEvent, useState } from 'react'
+import * as  z from 'zod'
+
 import { Input } from '../input'
 import { Button } from '../button'
 
+const schema = z.object({
+  title: z.string({ message: 'Titulo obrigatório' }).min(4),
+  details: z.string({ message: 'Detalhes obrigatório' }),
+  maximumAttendees: z.number({ message: 'Máximos de participantes obrigatório' }).int().positive(),
+  startDate: z.string({ message: 'Date de inicio obrigatório' }).min(10),
+  endDate: z.string({ message: 'Date de fim obrigatório' }).min(10),
+})
+
 export function EventForm() {
+  const [isLoading, setIsLoading] = useState(false)
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    setIsLoading(true)
+    event.preventDefault()
+    
+    const form = new FormData(event.currentTarget)
+
+    const title = form.get('title')
+    const details = form.get('details')
+    const maximumAttendees = form.get('maximumAttendees')
+    const startDate = form.get('startDate')
+    const endDate = form.get('endDate')
+
+    console.log(title)
+    console.log(details)
+    console.log(maximumAttendees)
+    console.log(startDate)
+    console.log(endDate)
+
+    setIsLoading(false)
+  }
+
   return (
-    <div>
+    <div className="flex flex-col items-center gap-4">
+
       <h1 className="text-2xl font-bold">
         Criar evento
       </h1>
 
-      <form className="flex flex-col gap-4">
+      <form  onSubmit={handleSubmit} className="flex flex-col items-center gap-4">
         <Input
-          id="title"
+          name="title"
           label="Titulo"
           iconName="minus"
         />
 
         <Input
-          id="details"
+          name="details"
           label="Detalhes"
           iconName="text"
         />
 
         <Input
-          id="maximumAttendees"
+          name="maximumAttendees"
           label="Máximos de participantes"
           iconName="users"
         />
 
+        <Input 
+          name="startDate"
+          label="Data de inicio"
+          iconName="calendar"
+          type="date"
+          />
+
+        <Input
+          name="endDate"
+          label="Data de fim"
+          iconName="calendar"
+          type="date"
+        />
+
         <Button
-          iconName="calendar-plus"
           type="submit"
+          iconName="calendar-plus"
+          variant="primary"
+          size="full"
+          isLoading={isLoading}
         >
-          Criar participante
+          Criar evento
         </ Button>
       </form>
     </div>
