4d3aa4f78798ebc550c5d7653442cd61836b072f
Bruno Becoski <brunobecoski@gmail.com>
2025-03-19 22:17:51 -0300

    Cria o componente UpdateEvent

--- src/components/events/event-details.tsx
+++ src/components/events/event-details.tsx
@@ -1,6 +1,5 @@
-import { ChangeEvent, FormEvent,useEffect, useState } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
 import dayjs from "dayjs"
-import { z } from "zod"
 
 import { useRouter } from "../../contexts/router-provider"
 import { 
@@ -9,10 +8,8 @@ import {
   deleteEventAttendee,
   EventAndAttendeesType,
   getEvent,
-  updateEvent
 } from "../../fetches"
 
-import { Input, InputVariants } from "../input"
 import { Table } from "../table/table"
 import { TableHeader } from "../table/table-header"
 import { TableRow } from "../table/table-row"
@@ -21,45 +18,15 @@ import { Button } from "../button"
 import { TableSearch } from "../table/table-search"
 import { TableFoot } from "../table/table-foot"
 import { AddAttendee } from "./add-attendee"
+import { UpdateEvent } from "./update-event"
 
-const schema = z.object({
-  title: z.string({ message: 'Titulo obrigatório' }).min(4, { message: 'Mínimo 4 caracteres' }),
-  details: z.string({ message: 'Detalhes obrigatório' }).min(4, { message: 'Mínimo 4 caracteres' }),
-  maximumAttendees: z.number({ message: 'Máximos de participantes obrigatório' }),
-  startDate: z.coerce.date({ message: 'Data de inicio obrigatório' }),
-  endDate: z.coerce.date({ message: 'Data de fim obrigatório' }),
-})
-
-interface FormStatusProps {
-  title: {
-    message: string
-    variant: InputVariants
-  }
-  details: {
-    message: string
-    variant: InputVariants
-  }
-  maximumAttendees: {
-    message: string
-    variant: InputVariants
-  }
-  startDate: {
-    message: string
-    variant: InputVariants
-  }
-  endDate: {
-    message: string
-    variant: InputVariants
-  }
-}
 
 export function EventDetails() {
   const [event, setEvent] = useState<EventAndAttendeesType>({} as EventAndAttendeesType)
   const [showForm, setShowForm] = useState(false)
-  const [formStatus, setFormStatus] = useState<FormStatusProps>({} as FormStatusProps)
-  const [isLoading, setIsLoading] = useState(false)
   const [isCheck, setIsCheck] = useState(false)
   const [isCheckArray, setIsCheckArray] = useState<string[]>([])
+  const [updateEventIsOpen, setUpdateEventIsOpen] = useState(false)
 
   const { slug, changeRoute, pageIndex, changePageIndex, search, changeSearch } = useRouter()
 
@@ -96,102 +63,6 @@ export function EventDetails() {
         alert(message)
         changeRoute({ route: 'events' })
       }
-    }
-  }
-
-  function formValidation(form: FormData) {
-    const title = form.get('title')
-    const details = form.get('details')
-    const maximumAttendees = Number(form.get('maximumAttendees'))
-    const startDate = form.get('startDate')
-    const endDate = form.get('endDate')
-
-    const result = schema.safeParse({
-      title,
-      details,
-      maximumAttendees,
-      startDate,
-      endDate,
-    })
-
-    if (result.success === false) {
-      const { title, details, maximumAttendees, startDate, endDate } = result.error.format()
-
-      setFormStatus({
-        title:  {
-          variant: title ? 'error' : 'success',
-          message: title?._errors[0] ? title._errors[0] : 'Titulo valido',
-        },
-        details: { 
-          variant: details ? 'error' : 'success',
-          message: details?._errors[0] ? details._errors[0] : 'Detalhes valido',
-        },
-        maximumAttendees: { 
-          variant: maximumAttendees ? 'error' : 'success',
-          message: maximumAttendees?._errors[0] ? maximumAttendees._errors[0] : 'Máximos de participantes valido',
-        },
-        startDate: { 
-          variant: startDate ? 'error' : 'success',
-          message: startDate?._errors[0] ? startDate._errors[0] : 'Data de inicio valido',
-        },
-        endDate: { 
-          variant: endDate ? 'error' : 'success',
-          message: endDate?._errors[0] ? endDate._errors[0] : 'Data de fim valido',
-        }
-      })
-
-    } else {
-      return {
-        title: result.data.title,
-        details: result.data.details,
-        maximumAttendees: result.data.maximumAttendees,
-        startDate: result.data.startDate,
-        endDate: result.data.endDate,
-      }
-    }
-  }
-
-  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
-    setIsLoading(true)
-
-    e.preventDefault()
-    
-    setFormStatus({} as FormStatusProps)
-
-    const form = new FormData(e.currentTarget)
-
-    const validatedForm = formValidation(form)
-
-    if (validatedForm === undefined) {
-      setIsLoading(false)
-
-      return
-    }
-
-    const { successfully, message, event: updatedEvent } = await updateEvent({
-      id: event.id,
-      title: validatedForm.title,
-      details: validatedForm.details,
-      maximumAttendees: validatedForm.maximumAttendees,
-      startDate: validatedForm.startDate,
-      endDate: validatedForm.endDate,
-    })
-
-    if (successfully == false || updatedEvent == undefined) {
-      setIsLoading(false)
-      alert(message)
-
-      return
-    }
-
-    if (successfully === true) {
-      setIsLoading(false)
-      setEvent({...event, title: updatedEvent.title, details: updatedEvent.details, maximumAttendees: updatedEvent.maximumAttendees, startDate: updatedEvent.startDate, endDate: updatedEvent.endDate  })
-      setShowForm(false)
-      changeRoute({ route: 'event', slug: updatedEvent.slug })
-      alert(message)
-
-      return
     }
   }
 
@@ -312,96 +183,41 @@ export function EventDetails() {
     <div className="space-y-6">
       <div className="flex justify-between">
         <div className="flex items-start justify-between w-full">
-
-        {
-          showForm ? (
-            <form  onSubmit={handleSubmit} className="flex flex-col items-center gap-4">
-              <Input
-                name="title"
-                label="Titulo"
-                iconName="type"
-                defaultValue={event.title}
-                message={formStatus.title?.message}
-                variant={formStatus.title?.variant}
-              />
-      
-              <Input
-                name="details"
-                label="Detalhes"
-                iconName="text"
-                defaultValue={event.details}
-                message={formStatus.details?.message}
-                variant={formStatus.details?.variant}
-              />
-      
-              <Input
-                name="maximumAttendees"
-                label="Máximos de participantes"
-                iconName="users"
-                defaultValue={event.maximumAttendees}
-                message={formStatus.maximumAttendees?.message}
-                variant={formStatus.maximumAttendees?.variant}
-              />
-      
-              <Input 
-                name="startDate"
-                label="Data de inicio"
-                iconName="calendar-days"
-                type="date"
-                defaultValue={event.startDate.split('T')[0]}
-                message={formStatus.startDate?.message}
-                variant={formStatus.startDate?.variant}
-              />
-      
-              <Input
-                name="endDate"
-                label="Data de fim"
-                iconName="calendar-days"
-                type="date"
-                defaultValue={event.endDate.split('T')[0]}
-                message={formStatus.endDate?.message}
-                variant={formStatus.endDate?.variant}
-              />
-      
-              <Button
-                type="submit"
-                iconName="calendar-plus"
-                variant="primary"
-                size="full"
-                isLoading={isLoading}
-              >
-                Atualizar evento
-              </ Button>
-            </form>
-          ) : (
-            <div className="flex flex-col gap-4">
-              <div className="flex items-center gap-10">
-                <h1 className="text-5xl font-bold italic">{event.title}</h1>
-                <Button title="Copiar" iconName="copy" onClick={() => navigator.clipboard.writeText(event.slug)}> {event.slug}</Button>
-              </div>
-
-              <div>
-                <span className="italic">{dayjs(event.startDate).format("DD/MM/YYYY")}</span>
-                <span> - </span>
-                <span className="italic">{dayjs(event.endDate).format("DD/MM/YYYY")}</span>
-              </div>
-              
-              <p className="text-lg">{event.details}</p>
-
-              <div className="flex flex-col gap-1 text-sm">
-                <span>Participantes: {event.totalAttendees}</span>
-                <span>Confirmado: {event.checkInAttendees}</span>
-                <span>Máximo: {event.maximumAttendees}</span>
-              </div>
+          <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-10">
+              <h1 className="text-5xl font-bold italic">{event.title}</h1>
+              <Button title="Copiar" iconName="copy" onClick={() => navigator.clipboard.writeText(event.slug)}> {event.slug}</Button>
             </div>
-          )
-        }
+
+            <div>
+              <span className="italic">{dayjs(event.startDate).format("DD/MM/YYYY")}</span>
+              <span> - </span>
+              <span className="italic">{dayjs(event.endDate).format("DD/MM/YYYY")}</span>
+            </div>
+            
+            <p className="text-lg">{event.details}</p>
+
+            <div className="flex flex-col gap-1 text-sm">
+              <span>Participantes: {event.totalAttendees}</span>
+              <span>Confirmado: {event.checkInAttendees}</span>
+              <span>Máximo: {event.maximumAttendees}</span>
+            </div>
+          </div>
+      
 
         <div className="flex flex-col items-end gap-4">
           <Button onClick={handleShowForm} iconName="pencil">Editar evento</Button>
           <Button onClick={handleDelete} variant="secondary" iconName="trash">Deletar evento</Button>
         </div>
       </div>
+    </div>
+
+    <div className="flex justify-between">
+      <AddAttendee slug={slug} fetchEvent={fetchEvent} />
+
+      <Button onClick={() => setUpdateEventIsOpen(true)}>
+        Atualizar evento
+      </Button>
     </div>
       {
         event.attendees?.length >= 1 ? (
@@ -412,8 +228,8 @@ export function EventDetails() {
                 search={search}
                 setSearch={changeSearch}
               />
-              
-              <AddAttendee slug={slug} fetchEvent={fetchEvent} />
+
+
             </div>
 
             {isCheck && 
@@ -521,6 +337,8 @@ export function EventDetails() {
           </>
         )
       }
+
+      <UpdateEvent isOpen={updateEventIsOpen}  setIsOpen={setUpdateEventIsOpen} event={event} setEvent={setEvent}/>
     </div>
   )
 }
---
+++ src/components/events/update-event.tsx
@@ -0,0 +1,242 @@
+import { FormEvent, useState } from 'react'
+import { tv, VariantProps } from 'tailwind-variants'
+import * as  z from 'zod'
+
+import { EventAndAttendeesType, updateEvent } from '../../fetches'
+import { useRouter } from '../../contexts/router-provider'
+
+import { Input, InputVariants } from '../input'
+import { Button } from '../button'
+
+const schema = z.object({
+  title: z.string({ message: 'Titulo obrigatório' }).min(4, { message: 'Mínimo 4 caracteres' }),
+  details: z.string({ message: 'Detalhes obrigatório' }).min(4, { message: 'Mínimo 4 caracteres' }),
+  maximumAttendees: z.number({ message: 'Máximos de participantes obrigatório' }),
+  startDate: z.coerce.date({ message: 'Data de inicio obrigatório' }),
+  endDate: z.coerce.date({ message: 'Data de fim obrigatório' }),
+})
+
+interface FormStatusProps {
+  title: {
+    message: string
+    variant: InputVariants
+  }
+  details: {
+    message: string
+    variant: InputVariants
+  }
+  maximumAttendees: {
+    message: string
+    variant: InputVariants
+  }
+  startDate: {
+    message: string
+    variant: InputVariants
+  }
+  endDate: {
+    message: string
+    variant: InputVariants
+  }
+}
+
+const div = tv({
+  base: 'flex flex-col justify-center items-center gap-4 fixed top-0 bottom-0 w-80 bg-zinc-950 border-l border-zinc-600 transition-all duration-500',
+
+  variants: {
+    isOpen: {
+      true: 'right-0',
+      false: '-right-[320px]',
+    }
+  }
+})
+
+interface UpdateEventProps extends VariantProps<typeof div> {
+  setIsOpen: (isOpen: boolean) => void
+  event: EventAndAttendeesType
+  setEvent: (event: EventAndAttendeesType) => void
+}
+
+export function UpdateEvent({ isOpen, setIsOpen, event, setEvent }: UpdateEventProps) {
+  const [formStatus, setFormStatus] = useState<FormStatusProps>({} as FormStatusProps)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const { changeRoute } = useRouter()
+
+  function formValidation(form: FormData) {
+    const title = form.get('title')
+    const details = form.get('details')
+    const maximumAttendees = Number(form.get('maximumAttendees'))
+    const startDate = form.get('startDate')
+    const endDate = form.get('endDate')
+
+    const result = schema.safeParse({
+      title,
+      details,
+      maximumAttendees,
+      startDate,
+      endDate,
+    })
+
+    if (result.success === false) {
+      const { title, details, maximumAttendees, startDate, endDate } = result.error.format()
+
+      setFormStatus({
+        title:  {
+          variant: title ? 'error' : 'success',
+          message: title?._errors[0] ? title._errors[0] : 'Titulo valido',
+        },
+        details: { 
+          variant: details ? 'error' : 'success',
+          message: details?._errors[0] ? details._errors[0] : 'Detalhes valido',
+        },
+        maximumAttendees: { 
+          variant: maximumAttendees ? 'error' : 'success',
+          message: maximumAttendees?._errors[0] ? maximumAttendees._errors[0] : 'Máximos de participantes valido',
+        },
+        startDate: { 
+          variant: startDate ? 'error' : 'success',
+          message: startDate?._errors[0] ? startDate._errors[0] : 'Data de inicio valido',
+        },
+        endDate: { 
+          variant: endDate ? 'error' : 'success',
+          message: endDate?._errors[0] ? endDate._errors[0] : 'Data de fim valido',
+        }
+      })
+
+    } else {
+      return {
+        title: result.data.title,
+        details: result.data.details,
+        maximumAttendees: result.data.maximumAttendees,
+        startDate: result.data.startDate,
+        endDate: result.data.endDate,
+      }
+    }
+  }
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    setIsLoading(true)
+
+    e.preventDefault()
+    
+    setFormStatus({} as FormStatusProps)
+
+    const form = new FormData(e.currentTarget)
+
+    const validatedForm = formValidation(form)
+
+    if (validatedForm === undefined) {
+      setIsLoading(false)
+
+      return
+    }
+
+    const { successfully, message, event: updatedEvent } = await updateEvent({
+      id: event.id,
+      title: validatedForm.title,
+      details: validatedForm.details,
+      maximumAttendees: validatedForm.maximumAttendees,
+      startDate: validatedForm.startDate,
+      endDate: validatedForm.endDate,
+    })
+
+    if (successfully == false || updatedEvent == undefined) {
+      setIsLoading(false)
+
+      alert(message)
+
+      return
+    }
+
+    if (successfully === true) {
+      setIsLoading(false)
+
+      alert(message)
+
+      setEvent({...event, title: updatedEvent.title, details: updatedEvent.details, maximumAttendees: updatedEvent.maximumAttendees, startDate: updatedEvent.startDate, endDate: updatedEvent.endDate  })
+      changeRoute({ route: 'event', slug: updatedEvent.slug })
+
+      setIsOpen(false)
+  
+      return
+    }
+  }
+
+  return (
+    <div className={div({ isOpen })}>
+      <div className="absolute top-5 right-5">
+        <Button
+          title="Fechar"
+          onClick={() => setIsOpen(false)}
+          iconName="arrow-right"
+          variant="close"
+          iconSize="default"
+        />
+      </div>
+
+      <form  onSubmit={handleSubmit} className="flex flex-col items-center justify-evenly py-30 h-full gap-4">
+        <h2 className="text-2xl font-bold">
+          Atualize o evento
+        </h2>
+  
+        <div className="flex flex-col gap-8">
+          <Input
+            name="title"
+            label="Titulo"
+            iconName="type"
+            message={formStatus.title?.message}
+            variant={formStatus.title?.variant}
+            defaultValue={event.title}
+            />
+
+          <Input
+            name="details"
+            label="Detalhes"
+            iconName="text"
+            message={formStatus.details?.message}
+            variant={formStatus.details?.variant}
+            defaultValue={event.details}
+          />
+
+          <Input
+            name="maximumAttendees"
+            label="Máximos de participantes"
+            iconName="users"
+            message={formStatus.maximumAttendees?.message}
+            variant={formStatus.maximumAttendees?.variant}
+            defaultValue={event.maximumAttendees}
+          />
+
+          <Input 
+            name="startDate"
+            label="Data de inicio"
+            iconName="calendar-days"
+            type="date"
+            message={formStatus.startDate?.message}
+            variant={formStatus.startDate?.variant}
+            defaultValue={event.startDate && event.startDate.split('T')[0]}
+          />
+
+          <Input
+            name="endDate"
+            label="Data de fim"
+            iconName="calendar-days"
+            type="date"
+            message={formStatus.endDate?.message}
+            variant={formStatus.endDate?.variant}
+            defaultValue={event.startDate && event.endDate.split('T')[0]}
+          />
+        </div>
+
+        <Button
+          type="submit"
+          variant="primary"
+          size="full"
+          isLoading={isLoading}
+        >
+          Atualizar evento
+        </ Button>
+      </form>
+    </div>
+  )
+}
